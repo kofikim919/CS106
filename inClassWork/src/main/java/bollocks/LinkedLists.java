e5f8a37b369062709a67c91c3cf12174362dada9
Kofi Kim <kofikim919@gmail.com>
2026-02-10 11:21:02 -0500

    Wow I added a lot really proud of my work today guys!!!!

--- inClassWork/src/main/java/bollocks/LinkedLists.java
+++ inClassWork/src/main/java/bollocks/LinkedLists.java
@@ -5,6 +5,7 @@ public class LinkedLists {
     public class LinkedList{
 
         Node head;
+        Node tail;
         private int size = 0;
 
         private class Node {
@@ -22,19 +23,57 @@ public class LinkedLists {
             Node newnode = list.new Node(element);
             //check if the head is null (meaning the linkedlist is empty)
             //if it is, set the head to the new node
+            //also set tail to new node
             if (list.head == null) {
                 list.head = newnode;
+                list.tail = newnode;
                 size++;
-            //if head is not null, follow the chain of nodes until you reach the end (next node would be null)
-            //and then make the new node the last node's next node
+            //if head is not null, go straight to the tail, and make its next node the new node
+            //then make the tail point to the new node
             } else {
-                Node current = list.head;
-                while (current.nextNode != null) {
-                    current = current.nextNode;
-                }
+                Node current = list.tail;
                 current.nextNode = newnode;
+                list.tail = newnode;
                 size++;
             }
+        }
+        // lets us add an element at a desired index
+        public void newNodeIndexed (LinkedList list, Object element, int position) {
+
+            Node newnode = list.new Node(element);
+            //if we want it at the start we make it the head and make the old head the next node
+            if (position==0) {
+                newnode.nextNode = list.head;
+                list.head = newnode;
+            //if we want it in the middle or end, we need to find the correct position
+            } else {
+                Node current = list.head;
+                for (int i = 0; i < position - 1; i++) {
+                    current = current.nextNode;
+                }
+                newnode.nextNode = current.nextNode;
+                current.nextNode = newnode;
+            }
+            list.size++;
+        }
+
+        public void removeNode (int index){
+
+            if (index<0 || index >= size) {
+                throw new IndexOutOfBoundsException("Invalid index");
+            }
+
+            if (index == 0) {
+                head = head.nextNode;
+            } else {
+
+                Node current = head;
+                for (int i = 0; i < index-1; i++) {
+                    current = current.nextNode;
+                }
+                current.nextNode = current.nextNode.nextNode;
+            }
+            size--;
         }
 
         public String printList() {
--- inClassWork/src/main/java/bollocks/Main.java
+++ inClassWork/src/main/java/bollocks/Main.java
@@ -7,8 +7,13 @@ public class Main {
         list.addNode(list, 4);
         list.addNode(list, "hello");
         list.addNode(list, 3.278);
+        list.addNode(list, "world");
+        list.addNode(list, "balls");
         System.out.println(list.printList());
         System.out.println(list.getNodeInfo(1));
+        list.newNodeIndexed(list, "of", 4);
+        list.removeNode(2);
+        System.out.println(list.printList());
     }
 
 }
