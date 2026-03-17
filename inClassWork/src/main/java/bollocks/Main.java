986d70943816884ccaeb7071831d9bc71f923725
Kofi Kim <kofikim919@gmail.com>
2026-03-17 10:39:17 -0400

    update

--- inClassWork/src/main/java/bollocks/Main.java
+++ inClassWork/src/main/java/bollocks/Main.java
@@ -2,17 +2,7 @@ package bollocks;
 
 public class Main {
     public static void main(String[] args) {
-
-        ArrayStack<Object> stack = new ArrayStack<>(5);
-        stack.push(4);
-        stack.push("hello");
-        stack.push(3.278);
-        stack.push("world");
-        stack.push("balls");
-        System.out.println(stack.printStack());
-        System.out.println(stack.pop());
-        stack.push("new");
-        System.out.println(stack.printStack());
+        
 
     }
 
